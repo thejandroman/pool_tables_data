693105c7cd6bf5ab1f45f92ca530810e4e81a48a
Alejandro Figueroa <alejandro@ideasftw.com>
2023-12-17 14:51:13 -0400

    Add imports to pool.js

--- src/pool.js
+++ src/pool.js
@@ -1,3 +1,8 @@
+import "jquery";
+import "bootstrap";
+import "bootstrap-table/dist/bootstrap-table.min.js";
+import "bootstrap-table/dist/locale/bootstrap-table-en-US.min.js";
+
 $.getJSON("data/san_francisco.json", function (data) {
   $("#table").bootstrapTable({
     data: data,
